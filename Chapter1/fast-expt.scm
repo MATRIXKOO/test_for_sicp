4bff4fbd44c1dd0d23f638922561b0aa0201f082
MATRIXKOO <matrixkook@outlook.com>
2020-06-15 11:14:55 +0800

    e1_15 - e1_17

---
+++ Chapter1/fast-expt.scm
@@ -0,0 +1,17 @@
+(define (fast-expt b n)
+(cond ((= n 0) 1)
+((even? n) (square (fast-expt b (/ n 2))))
+(else (* b (fast-expt b (- n 1))))))
+
+
+(define (expt b n)
+(expt-iter b n 1))
+(define (expt-iter b counter product)
+(if (= counter 0)
+product
+(expt-iter b
+(- counter 1)
+(* b product))))
+
+;(fast-expt 2 20) ; 0.097s
+(expt 2 20) ; 0.102s
