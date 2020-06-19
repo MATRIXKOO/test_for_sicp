48867e064e3bcdfc6ad17f3a5365562e0c62256f
MATRIXKOO <matrixkook@outlook.com>
2020-06-19 09:40:46 +0800

    [ADD]: e1_20 - e1_23

---
+++ Chapter1/e1_23.scm
@@ -0,0 +1,60 @@
+;; ex 1.23 
+  
+ (define (square x) (* x x)) 
+  
+ (define (smallest-divisor n) 
+   (find-divisor n 2)) 
+  
+ (define (find-divisor n test-divisor) 
+   (define (next n) 
+     (if (= n 2) 3 (+ n 2))) 
+   (cond ((> (square test-divisor) n) n) 
+         ((divides? test-divisor n) test-divisor) 
+         (else (find-divisor n (next test-divisor))))) 
+  
+ (define (divides? a b) 
+   (= (remainder b a) 0)) 
+  
+ (define (prime? n) 
+   (= n (smallest-divisor n))) 
+  
+ (define (timed-prime-test n) 
+   (start-prime-test n (runtime))) 
+  
+ (define (start-prime-test n start-time) 
+   (if (prime? n) 
+       (report-prime n (- (runtime) start-time)))) 
+  
+ (define (report-prime n elapsed-time) 
+   (newline) 
+   (display n) 
+   (display " *** ") 
+   (display elapsed-time)) 
+  
+ (timed-prime-test 1009) 
+ (timed-prime-test 1013) 
+ (timed-prime-test 1019) 
+ (timed-prime-test 10007) 
+ (timed-prime-test 10009) 
+ (timed-prime-test 10037) 
+ (timed-prime-test 100003) 
+ (timed-prime-test 100019) 
+ (timed-prime-test 100043) 
+ (timed-prime-test 1000003) 
+ (timed-prime-test 1000033) 
+ (timed-prime-test 1000037) 
+  
+ ; See comments in exercise 1.22 
+ (newline) 
+ (timed-prime-test 1000000007) 
+ (timed-prime-test 1000000009) 
+ (timed-prime-test 1000000021) 
+ (timed-prime-test 10000000019) 
+ (timed-prime-test 10000000033) 
+ (timed-prime-test 10000000061) 
+ (timed-prime-test 100000000003) 
+ (timed-prime-test 100000000019) 
+ (timed-prime-test 100000000057) 
+ (timed-prime-test 1000000000039) 
+ (timed-prime-test 1000000000061) 
+ (timed-prime-test 1000000000063) 
